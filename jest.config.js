fa7d79f03fb510e3f2290a3d7e76fe00404b1758
HuiDong Wu <wuhd@cn.ibm.com>
2020-01-30 19:12:41 +0800

    edit

--- jest.config.js
+++ jest.config.js
@@ -97,7 +97,8 @@ module.exports = {
   // projects: null,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: ["default", "jest-html-reporters"],
+
 
   // Automatically reset mock state between every test
   // resetMocks: false,
